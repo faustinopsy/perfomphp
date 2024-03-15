9836ddb30184fc76267cb7592e9cc352e0a0ff26
faustinopsy <rodrigohipnose@gmail.com>
2024-03-15 13:56:41 -0300

    simplificando a rota

--- App/index.php
+++ App/index.php
@@ -8,36 +8,46 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit(0);
 }
-$data = json_decode(file_get_contents("php://input"));
 
-if (
-    isset($data->num1, $data->num2, $data->num3, $data->num4, $data->num5, $data->num6) &&
-    is_numeric($data->num1) &&
-    is_numeric($data->num2) &&
-    is_numeric($data->num3) &&
-    is_numeric($data->num4) &&
-    is_numeric($data->num5) &&
-    is_numeric($data->num6)
-) {
-    $mega = new Mega();
-    $mega->setNum1(intval($data->num1));
-    $mega->setNum2(intval($data->num2));
-    $mega->setNum3(intval($data->num3));
-    $mega->setNum4(intval($data->num4));
-    $mega->setNum5(intval($data->num5));
-    $mega->setNum6(intval($data->num6));
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    if ($mega->insertMega()) {
-        echo json_encode(["message" => "Dados inseridos com sucesso."]);
-    } else {
-        echo json_encode(["message" => "Falha ao inserir dados."]);
+    $data = json_decode(file_get_contents("php://input"));
+
+    function isValid($data) {
+        $requiredFields = ['num1', 'num2', 'num3', 'num4', 'num5', 'num6'];
+        foreach ($requiredFields as $field) {
+            if (!isset($data->$field) || !is_numeric($data->$field)) {
+                return false;
+            }
+        }
+        return true;
     }
-} else {
-    http_response_code(400);
-    echo json_encode(["error" => "Dados de entrada inválidos."]);
-    exit;
+
+    if (isValid($data)) {
+        $mega = new Mega();
+        
+        $mega->setNum1(intval($data->num1));
+        $mega->setNum2(intval($data->num2));
+        $mega->setNum3(intval($data->num3));
+        $mega->setNum4(intval($data->num4));
+        $mega->setNum5(intval($data->num5));
+        $mega->setNum6(intval($data->num6));
+
+        if ($mega->insertMega()) {
+            http_response_code(200); 
+            echo json_encode(["message" => "Dados inseridos com sucesso."]);
+        } else {
+            http_response_code(500); 
+            echo json_encode(["message" => "Falha ao inserir dados."]);
+        }
+    } else {
+        http_response_code(400); 
+        echo json_encode(["error" => "Dados de entrada inválidos."]);
+        exit;
+    }
 }
-?>
+
