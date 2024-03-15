b16a85732bbe388b16f1a2e320345567745055bf
faustinopsy <rodrigohipnose@gmail.com>
2024-03-15 14:06:17 -0300

    alterando a rota para buscar id e todos

--- App/index.php
+++ App/index.php
@@ -5,7 +5,7 @@ use App\Model\Mega;
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
@@ -13,21 +13,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit(0);
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'POST':
+        $data = json_decode(file_get_contents("php://input"));
 
-    $data = json_decode(file_get_contents("php://input"));
-
-    function isValid($data) {
-        $requiredFields = ['num1', 'num2', 'num3', 'num4', 'num5', 'num6'];
-        foreach ($requiredFields as $field) {
-            if (!isset($data->$field) || !is_numeric($data->$field)) {
-                return false;
-            }
+        if (!isValid($data)) {
+            http_response_code(400);
+            echo json_encode(["error" => "Dados de entrada inválidos."]);
+            break;
         }
-        return true;
-    }
 
-    if (isValid($data)) {
         $mega = new Mega();
         
         $mega->setNum1(intval($data->num1));
@@ -38,16 +33,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $mega->setNum6(intval($data->num6));
 
         if ($mega->insertMega()) {
-            http_response_code(200); 
+            http_response_code(200);
             echo json_encode(["message" => "Dados inseridos com sucesso."]);
         } else {
-            http_response_code(500); 
+            http_response_code(500);
             echo json_encode(["message" => "Falha ao inserir dados."]);
         }
-    } else {
-        http_response_code(400); 
-        echo json_encode(["error" => "Dados de entrada inválidos."]);
-        exit;
-    }
+        break;
+    case 'GET':
+        $mega = new Mega();
+        if (isset($_GET['id'])) {
+            $result = $mega->getById(intval($_GET['id']));
+        } else {
+            $result = $mega->getAll();
+        }
+
+        if ($result) {
+            http_response_code(200);
+            echo json_encode($result);
+        } else {
+            http_response_code(404);
+            echo json_encode(["message" => "Nenhum dado encontrado."]);
+        }
+        break;
+    default:
+        http_response_code(405);
+        echo json_encode(["error" => "Método não permitido."]);
+        break;
 }
 
+function isValid($data) {
+    $requiredFields = ['num1', 'num2', 'num3', 'num4', 'num5', 'num6'];
+    foreach ($requiredFields as $field) {
+        if (!isset($data->$field) || !is_numeric($data->$field)) {
+            return false;
+        }
+    }
+    return true;
+}
